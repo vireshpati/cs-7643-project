02a67742b4a215f76e75f7abf93902fac9f810c6
vireshpati <viresh@patifamily.com>
2025-10-27 17:40:40 -0400

    configured transformer scripts and small grid search

---
+++ Time-Series-Library/scripts/long_term_forecast/ETT_script/Transformer_ETTh1_grid.sh
@@ -0,0 +1,40 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=Transformer
+
+# Grid Search
+pred_len=(96 192 336 720)
+e_layers=(2)
+d_layers=(1)
+
+
+
+for pred_len in ${pred_len[@]}; do
+    for e_layers in ${e_layers[@]}; do
+        for d_layers in ${d_layers[@]}; do
+            python -u run.py \
+                --task_name long_term_forecast \
+                --is_training 1 \
+                --root_path ./dataset/ETT-small/ \
+                --data_path ETTh1.csv \
+                --model_id ETTh1_96_${pred_len} \
+                --model $model_name \
+                --data ETTh1 \
+                --features M \
+                --seq_len 96 \
+                --label_len 48 \
+                --pred_len ${pred_len} \
+                --e_layers ${e_layers} \
+                --d_layers ${d_layers} \
+                --factor 3 \
+                --enc_in 7 \
+                --dec_in 7 \
+                --c_out 7 \
+                --des 'Exp' \
+                --itr 1
+        done
+    done
+done
+
+
+
