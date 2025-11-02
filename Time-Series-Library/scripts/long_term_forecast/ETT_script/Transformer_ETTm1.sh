52df7d3edcc15d092f724ed3c2964647fa2337ad
vireshpati <viresh@patifamily.com>
2025-11-02 18:17:07 -0500

    transformer scripts

--- Time-Series-Library/scripts/long_term_forecast/ETT_script/Transformer_ETTm1.sh
+++ Time-Series-Library/scripts/long_term_forecast/ETT_script/Transformer_ETTm1.sh
@@ -2,82 +2,59 @@ export CUDA_VISIBLE_DEVICES=0
 
 model_name=Transformer
 
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTm1.csv \
-  --model_id ETTm1_96_96 \
-  --model $model_name \
-  --data ETTm1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 96 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1
 
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTm1.csv \
-  --model_id ETTm1_96_192 \
-  --model $model_name \
-  --data ETTm1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 192 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1
+pred_len_list=(96 192 336 720)
 
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTm1.csv \
-  --model_id ETTm1_96_336 \
-  --model $model_name \
-  --data ETTm1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 336 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1
+e_layers_list=(2)
+n_heads_list=(8)
+d_model_list=(128)
 
-python -u run.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTm1.csv \
-  --model_id ETTm1_96_720 \
-  --model $model_name \
-  --data ETTm1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 720 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1
+dropout_list=(0.1)
+batch_size_list=(64)
+learning_rate_list=(0.0001)
+
+
+for pred_len in "${pred_len_list[@]}"; do
+  for e_layers in "${e_layers_list[@]}"; do
+    for n_heads in "${n_heads_list[@]}"; do
+      for d_model in "${d_model_list[@]}"; do
+        for dropout in "${dropout_list[@]}"; do
+          for batch_size in "${batch_size_list[@]}"; do
+            for learning_rate in "${learning_rate_list[@]}"; do
+              python -u run.py \
+                --task_name long_term_forecast \
+                --is_training 1 \
+                --root_path ./dataset/ETT-small/ \
+                --data_path ETTm1.csv \
+                --model_id ETTm1_96_96 \
+                --model $model_name \
+                --data ETTm1 \
+                --features M \
+                --seq_len 96 \
+                --label_len 48 \
+                --pred_len $pred_len \
+                --e_layers $e_layers \
+                --d_model $d_model \
+                --d_ff $((d_model * 4)) \
+                --n_heads $n_heads \
+                --dropout $dropout \
+                --batch_size $batch_size \
+                --learning_rate $learning_rate \
+                --d_layers 1 \
+                --factor 3 \
+                --enc_in 7 \
+                --dec_in 7 \
+                --c_out 7 \
+                --des 'Exp' \
+                --itr 1 \
+                --train_epochs 100 \
+                --patience 5 
+            done
+          done
+        done
+      done
+    done
+  done
+done
+
+
